4b47b006bb5730c7f7f7fa77063675ffbbcde205
Seda Gültekin <sedagulteknn@gmail.com>
2022-09-26 10:48:07 +0300

    Initial Commit

---
+++ madduck_project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  madduck_project
+//
+//  Created by Seda Gültekin on 22.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ madduck_projectUITests/madduck_projectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  madduck_projectUITestsLaunchTests.swift
+//  madduck_projectUITests
+//
+//  Created by Seda Gültekin on 22.09.2022.
+//
+
+import XCTest
+
+class madduck_projectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
